ba964369dc45caead3342c1d617522c3d0230465
Haydar Alkaduhimi <haydar@developing4all.com>
2026-05-17 13:35:11 +0200

    Added  a small space after the control buttons to be a draggable area if there is no space on the tabbar

--- src/ui/WindowControlsWidget.cpp
+++ src/ui/WindowControlsWidget.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2026 Water Phoenix Browser contributors
+* Copyright (C) 2026 Haydar Alkaduhimi <haydar@developing4all.com>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
